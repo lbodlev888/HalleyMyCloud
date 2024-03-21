218362f2cd170dca3a9d59699992a240ba727b78
Bodlev Laurentiu <lbodlev888@gmail.com>
2024-03-21 20:54:21 +0200

    Procedura de descarcare mai corecta

--- site/upload.php
+++ site/upload.php
@@ -14,11 +14,12 @@
     $email = $_SESSION['user'];
     $now = strval(date('Y-m-d H:i:s'));
     $full_gen_name = $genName.".".strtolower(pathinfo($filename, PATHINFO_EXTENSION));
-    $sql = "INSERT INTO files (genName, fileName, emailUser, creationDate) VALUES ('$full_gen_name', '$filename', '$email', '$now')";
-    $conn->query($sql);
 
     $target_path = "/var/www/uploads/".$full_gen_name;
-    if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path))
+    if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+        $sql = "INSERT INTO files (genName, fileName, emailUser, creationDate) VALUES ('$full_gen_name', '$filename', '$email', '$now')";
+        $conn->query($sql);
         header('Location: index.php?uploaded=ok');
+    }
     else header('Location: index.php?uploaded=failed');
 ?>
